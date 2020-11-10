c184cd66f932975ff0cd6ec61c1887f30b2c53ad
Nils Reichardt <nils@sharezone.net>
2020-11-10 12:52:00 +0100

    Update VigenereChiffre.java

--- VigenereChiffre.java
+++ VigenereChiffre.java
@@ -1,9 +1,6 @@
-public class VigenereWithoutArray
+public class VigenereChiffre
 {
-    /**
-     * @param text Text, der entweder verschlüsselt oder entschlüsselt werden soll.
-     */
-    public String entschluesseln(String text, String key, boolean wirdVerschlusselt) {
+    public String encrytAndDecrypt(String text, String key, boolean shouldDecrypt) {
         text = text.toUpperCase().trim();
         key = key.toUpperCase().trim();
 
@@ -11,7 +8,7 @@ public class VigenereWithoutArray
         for(int i = 0, j = 0; i < text.length(); i++, j++) {
             if(j == key.length()) j = 0;
 
-            if(wirdVerschlusselt)
+            if(shouldDecrypt)
                 result += (char)((text.charAt(i) - key.charAt(j) - 130) % 26 + 65);
             else
                 result += (char)((text.charAt(i) - key.charAt(j) + 26) % 26 + 65);
